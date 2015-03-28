3f7048d57e47ca9afd6ce7324c21d60f71baaa31
Brian <bcf.vandenbroek@student.avans.nl>
2015-03-28 23:14:08 +0100

    paginering bij home assortiment

--- templates_c/2bc4fc92c46d5a6e247909c72749e232130c815b.file.assortment.tpl.php
+++ templates_c/2bc4fc92c46d5a6e247909c72749e232130c815b.file.assortment.tpl.php
@@ -1,4 +1,4 @@
-<?php /* Smarty version Smarty-3.1.21-dev, created on 2015-03-28 18:59:37
+<?php /* Smarty version Smarty-3.1.21-dev, created on 2015-03-28 23:11:32
          compiled from "app\view\home\assortment.tpl" */ ?>
 <?php /*%%SmartyHeaderCode:125465516e6069d0274-81963990%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
 $_valid = $_smarty_tpl->decodeProperties(array (
@@ -7,7 +7,7 @@ $_valid = $_smarty_tpl->decodeProperties(array (
     '2bc4fc92c46d5a6e247909c72749e232130c815b' => 
     array (
       0 => 'app\\view\\home\\assortment.tpl',
-      1 => 1427565570,
+      1 => 1427580689,
       2 => 'file',
     ),
   ),
@@ -27,6 +27,9 @@ $_valid = $_smarty_tpl->decodeProperties(array (
     'products' => 0,
     'product' => 0,
     'last_product' => 0,
+    'pages' => 0,
+    'current_page' => 0,
+    'p' => 0,
   ),
   'has_nocache_code' => false,
 ),false); /*/%%SmartyHeaderCode%%*/?>
@@ -142,6 +145,45 @@ _1.png" alt="auction">
 					<?php }?>
 				</div>
 			</div>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-12">
+			<nav class="pull-right">
+				<ul class="pagination pagination-sm">
+					<?php if ($_smarty_tpl->tpl_vars['pages']->value>1) {?>
+					<?php if ($_smarty_tpl->tpl_vars['current_page']->value>1) {?>
+						<li><a href="/home/assortment?page=<?php echo $_smarty_tpl->tpl_vars['current_page']->value-1;
+if (isset($_smarty_tpl->tpl_vars['currcategory']->value)) {?>&categorie=<?php echo $_smarty_tpl->tpl_vars['currcategory']->value;
+}?>" aria-label="Previous"><span aria-hidden="true">&laquo;</span></a></li>
+					<?php } else { ?>
+						<li class="disabled"><a href="#" aria-label="Previous"><span aria-hidden="true">&laquo;</span></a></li>
+					<?php }?>
+					<?php $_smarty_tpl->tpl_vars['p'] = new Smarty_Variable;$_smarty_tpl->tpl_vars['p']->step = 1;$_smarty_tpl->tpl_vars['p']->total = (int) ceil(($_smarty_tpl->tpl_vars['p']->step > 0 ? $_smarty_tpl->tpl_vars['pages']->value+1 - (1) : 1-($_smarty_tpl->tpl_vars['pages']->value)+1)/abs($_smarty_tpl->tpl_vars['p']->step));
+if ($_smarty_tpl->tpl_vars['p']->total > 0) {
+for ($_smarty_tpl->tpl_vars['p']->value = 1, $_smarty_tpl->tpl_vars['p']->iteration = 1;$_smarty_tpl->tpl_vars['p']->iteration <= $_smarty_tpl->tpl_vars['p']->total;$_smarty_tpl->tpl_vars['p']->value += $_smarty_tpl->tpl_vars['p']->step, $_smarty_tpl->tpl_vars['p']->iteration++) {
+$_smarty_tpl->tpl_vars['p']->first = $_smarty_tpl->tpl_vars['p']->iteration == 1;$_smarty_tpl->tpl_vars['p']->last = $_smarty_tpl->tpl_vars['p']->iteration == $_smarty_tpl->tpl_vars['p']->total;?>
+					<?php if ($_smarty_tpl->tpl_vars['p']->value==$_smarty_tpl->tpl_vars['current_page']->value) {?>
+					<li class="active"><a href="/home/assortment?page=$p<?php if (isset($_smarty_tpl->tpl_vars['currcategory']->value)) {?>&categorie=<?php echo $_smarty_tpl->tpl_vars['currcategory']->value;
+}?>"><?php echo $_smarty_tpl->tpl_vars['p']->value;?>
+</a></li>
+					<?php } else { ?>
+					<li><a href="/home/assortment?page=<?php echo $_smarty_tpl->tpl_vars['p']->value;
+if (isset($_smarty_tpl->tpl_vars['currcategory']->value)) {?>&categorie=<?php echo $_smarty_tpl->tpl_vars['currcategory']->value;
+}?>"><?php echo $_smarty_tpl->tpl_vars['p']->value;?>
+</a></li>
+					<?php }?>
+					<?php }} ?>
+					<?php if ($_smarty_tpl->tpl_vars['pages']->value==$_smarty_tpl->tpl_vars['current_page']->value) {?>
+					<li class="disabled"><a href="#">&raquo;</a></li>
+					<?php } else { ?>
+					<li><a href="/home/assortment?page=<?php echo $_smarty_tpl->tpl_vars['current_page']->value+1;
+if (isset($_smarty_tpl->tpl_vars['currcategory']->value)) {?>&categorie=<?php echo $_smarty_tpl->tpl_vars['currcategory']->value;
+}?>">&raquo;</a></li>
+					<?php }?>
+					<?php }?>
+				</ul>
+			</nav>
 		</div>
 	</div>
 </div><?php }} ?>
