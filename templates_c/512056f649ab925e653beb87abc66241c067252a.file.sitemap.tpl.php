e1b13276fb6708cd6248dfbe7b3725191713fe8c
stanvanheumen <cjgheume@student.avans.nl>
2015-03-30 19:37:52 +0200

    sitemap foute links

--- templates_c/512056f649ab925e653beb87abc66241c067252a.file.sitemap.tpl.php
+++ templates_c/512056f649ab925e653beb87abc66241c067252a.file.sitemap.tpl.php
@@ -1,4 +1,25 @@
-<div class="container">
+<?php /* Smarty version Smarty-3.1.21-dev, created on 2015-03-30 19:37:17
+         compiled from "app\view\\home\sitemap.tpl" */ ?>
+<?php /*%%SmartyHeaderCode:153765519894a3e45e9-64534917%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+$_valid = $_smarty_tpl->decodeProperties(array (
+  'file_dependency' => 
+  array (
+    '512056f649ab925e653beb87abc66241c067252a' => 
+    array (
+      0 => 'app\\view\\\\home\\sitemap.tpl',
+      1 => 1427737036,
+      2 => 'file',
+    ),
+  ),
+  'nocache_hash' => '153765519894a3e45e9-64534917',
+  'function' => 
+  array (
+  ),
+  'version' => 'Smarty-3.1.21-dev',
+  'unifunc' => 'content_5519894a48b7c6_68133741',
+  'has_nocache_code' => false,
+),false); /*/%%SmartyHeaderCode%%*/?>
+<?php if ($_valid && !is_callable('content_5519894a48b7c6_68133741')) {function content_5519894a48b7c6_68133741($_smarty_tpl) {?><div class="container">
 	<div class="panel panel-primary">
 		<div class="panel-heading">
 			<span class="panel-title">Sitemap</span>
@@ -41,4 +62,4 @@
 			</tbody>
 		</table>
 	</div>
-</div>
+</div><?php }} ?>
